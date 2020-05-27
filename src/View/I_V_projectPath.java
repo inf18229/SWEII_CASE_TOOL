c6580248072f73eed6cb572b671eb32154f87579
Florian Schiffel <fschiffel@googlemail.com>
2020-05-27 17:01:51 +0200

    added FileChooser

--- src/View/I_V_projectPath.java
+++
@@ -1,6 +0,0 @@
-package View;
-
-public interface I_V_projectPath {
-    public String getPath();
-    //public void actionPerformed (ActionEvent ae);
-}
--- src/View/I_V_start.java
+++ src/View/I_V_start.java
@@ -1,8 +1,6 @@
 package View;
 
-import Controller.C_start;
-
 public interface I_V_start {
     public void debugPrint(String output);
-    //TODO: Interface mit einbeziehen
+    public String getPath();
 }
