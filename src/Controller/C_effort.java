cee70baf50a13961353d7511c4cb8faeb83b764c
Florian Schiffel <fschiffel@googlemail.com>
2020-05-28 17:37:08 +0200

    C_effort: added coments and improved consistency

--- src/Controller/C_effort.java
+++ src/Controller/C_effort.java
@@ -2,12 +2,16 @@ package Controller;
 
 import View.I_V_frame;
 
+/**
+ * is the implementation of I_C_effort
+ * this Controller has to manage all action regarding the "Aufwandsschätzung" tab
+ */
 public class C_effort implements I_C_effort{
-    I_V_frame viewFrame;
+
+    I_V_frame viewFrame;    //stores a reference to the viewFrame to update elements of it
 
     public C_effort(I_V_frame view){
         viewFrame = view;
-
     }
 
     /**
@@ -16,21 +20,12 @@ public class C_effort implements I_C_effort{
      */
     @Override
     public void updateView() {
-
-    }
-
-    /**
-     * method to simply print the string output as text to console
-     *
-     * @param output
-     */
-    @Override
-    public void debugPrint(String output) {
-        System.out.println(output);
+        //TODO: add implementation
     }
 
     @Override
     public void updateProjectData(){
-
+        //TODO: clarify: what should be done in this method?
+        //TODO: add implementation
     }
 }
