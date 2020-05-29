ab1f2a43418628d61ebace17059816cb908cac8e
Florian Schiffel <fschiffel@googlemail.com>
2020-05-29 10:33:48 +0200

    added comments and made new java doc

--- src/main/java/DEMO/TESTDEMO.java
+++ src/main/java/DEMO/TESTDEMO.java
@@ -1,30 +1,21 @@
+package DEMO;
+
 /**
- *  The class or method name - TESTDEMO
- *
  *  Description of the class or method purpose:
  *  The point of this demo class is to show how to comment the code, reuse basic comment fragments to create a pleasing
  *  and easy to read code.                                                                                               <-- this line shall never be passed when commenting
- *                                                                                                                            or writing code
- *  TODO: Decide which comment format to use /* easier to use, // nicer, /* also used for javadoc
+ *  There is no need to write the method name into this description                                                          or writing code
  */
-
-package DEMO;
 public class TESTDEMO {
     //TODO: This is some basic todo. TODOs should always created, that any other coder would be able to fulfill it.
     //TODO: Splitting up todos in smaller packages makes fulfilling easier and creates more pleasing code
 
-    public  void demoMethod(){
-        ///////////////
-        //  demoMethod
-        //
-        //  The demoMethod shows how to correctly comment and create a new Method
-        //
-        //  Input:
-        //  The demoMethod doesn't get any input...
-        //
-        //  Output:
-        //  ...neither does it produce any output, it is therefore useless, should never be called and just just serves
-        //  the purpose of showing how to use basic comments.
-        ///////////////
+    /**
+     * The demoMethod shows how to correctly comment and create a new Method
+     * @param param1 descriction
+     * @return 0
+     */
+    public  int demoMethod(int param1, int param2){
+        return 0;
     }
 }
