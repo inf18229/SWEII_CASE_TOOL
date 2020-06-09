58d464854c61fc4cb6cc2b92a1e0927745343002
Florian Schiffel <fschiffel@googlemail.com>
2020-06-09 12:53:19 +0200

    created javadoc

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,15 +12,12 @@ public class main {
 }
 
 //================================================wichtige Änderungen===================================================
-//TODO: change Open Dialogue -> no file should be selected when opening
-// wenn ich nen neues Projekt anlegen will, habe ich ja noch keine XML-Datei?
 //TODO: Information hiding - variable prefix public/private aktualisieren
 //TODO: add Interfaces to the Model-Classes
 //TODO: combine export and import into one file with export and import methods? if not -> Begründung in Doku
 //TODO: Maven???
+
 //TODO: rename packages and classes in CAPSLOCK
-//TODO: Optimierung nicht vergessen
-//TODO: add notifyButton(int id); which calls the required functions depending on the id
 //TODO: refactor funcReq to product function
 
 //TODO: check variable names (english or german?)
@@ -32,3 +29,4 @@ public class main {
 //TODO: Verwirrung zwischen neu und speichern welcher Knopf bewirkt was -> besser darstellen bloß wie?
 //TODO: löschen Knopf entfernen/deaktivieren, wenn Liste leer
 //TODO: entscheiden in welcher Reihenfolge paramter übergeben werden (model, view, controller oder anders?)
+//TODO: add notifyButton(int id); which calls the required functions depending on the id
